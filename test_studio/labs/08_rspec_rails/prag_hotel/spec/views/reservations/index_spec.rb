e2827ed221f1c1714cba01262d677f427295f190
Joe O'Brien <joe@theedgecase.com>
2008-05-22 15:32:57 -0700

    adding in changes to rspec lab and todo notes

--- test_studio/labs/08_rspec_rails/prag_hotel/spec/views/reservations/index_spec.rb
+++ test_studio/labs/08_rspec_rails/prag_hotel/spec/views/reservations/index_spec.rb
@@ -2,8 +2,10 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "reservations/index" do
   before :each do
-    @reservation_one = flexmock(:model, Reservation, Reservation.valid_options)
-    @reservation_two = flexmock(:model, Reservation, Reservation.valid_options)
+    @reservation_one = flexmock(:model,
+      Reservation, Reservation.valid_options)
+    @reservation_two = flexmock(:model,
+      Reservation, Reservation.valid_options)
 
     assigns[:reservations] = [ @reservation_one, @reservation_two ]
   end

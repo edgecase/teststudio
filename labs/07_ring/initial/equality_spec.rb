67eeafa9075bbd45c91eae5a91682f4b034a07cd
Jim Weirich <jim.weirich@gmail.com>
2008-05-21 22:17:55 -0700

    added intiial file

---
+++ labs/07_ring/initial/equality_spec.rb
@@ -0,0 +1,34 @@
+
+describe "equality checks" do
+  
+  it "should determine structrual equality" do
+    a = [1,2]
+    b = [1,2]
+
+    a.should == b
+    a.should_not be_equal(b)
+    a.should_not be(b)
+  end
+
+
+  it "should determine identity equality" do
+    a = [1,2]
+    b = a
+
+    a.should be(b)
+    a.should be_equal(b)
+  end
+
+  it "should be type equal" do
+    a = 1
+    b = 1.0
+
+    a.should  == b
+    a.should_not be_eql(b)
+    a.should_not be_equal(b)
+    a.should_not be(b)
+  end
+
+end
+
+

a6876d8d11b642af39f9b37acffd7520d1469d01
Jim Weirich <jim.weirich@gmail.com>
2010-02-28 16:50:35 -0500

    removed unneeded controller actions

--- labs/greed_solution/app/controllers/interactive_turns_controller.rb
+++ labs/greed_solution/app/controllers/interactive_turns_controller.rb
@@ -1,24 +1,4 @@
 class InteractiveTurnsController < ApplicationController
-  def computer_turn
-    setup_page_data
-    cp = @game.computer_player
-    cp.roller = roller
-    turn = cp.take_turn
-    cp.score += turn.score
-    cp.save
-    redirect_to computer_turn_results_non_interactive_turn_path(@game)
-  end
-
-  def computer_turn_results
-    setup_page_data
-    if @game.computer_player.score >= 3000
-      @winner = @game.computer_player.name
-      render :template => "common/game_over"
-    else
-      @most_recent_turn = [@game.computer_player.turns.last]
-    end
-  end
-
   def human_start_turn
     setup_page_data
     @game.human_player.roller = roller

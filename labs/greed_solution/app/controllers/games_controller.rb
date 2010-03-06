5d76bc162cc0bfc866b886f02f515b522e0b687b
Jim Weirich <jim.weirich@gmail.com>
2010-03-06 16:00:17 -0500

    removed obsolete methods

--- labs/greed_solution/app/controllers/games_controller.rb
+++ labs/greed_solution/app/controllers/games_controller.rb
@@ -15,24 +15,4 @@ class GamesController < ApplicationController
       redirect_to new_game_path
     end
   end
-
-  def choose_players
-    @game = Game.find(params[:id])
-    @players = AutoPlayer.players
-  end
-
-  def assign_players
-    @game = Game.find(params[:id])
-    strategy_name = params[:player] || []
-    if strategy_name.blank?
-      flash[:error] = "Please select at least one computer player"
-      redirect_to choose_players_game_path(@game)
-    else
-      cp = ComputerPlayer.new(:strategy => strategy_name, :position => 1)
-      @game.players << cp
-      @game.current_player = @game.players.first
-      @game.save
-      redirect_to start_turn_path(@game)
-    end
-  end
 end

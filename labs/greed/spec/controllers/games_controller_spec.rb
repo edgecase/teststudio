f7e9bead3b9c4afe759491c9bd642ff35c33e622
Jim Weirich <jim.weirich@gmail.com>
2010-10-14 14:44:26 -0400

    Now with let! goodness.

--- labs/greed/spec/controllers/games_controller_spec.rb
+++ labs/greed/spec/controllers/games_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe GamesController do
+
   describe "GET new" do
     it "renders the new view" do
       get :new
@@ -9,30 +10,34 @@ describe GamesController do
   end
 
   describe "POST create" do
-    before do
-      @game = Factory.build(:game)
-      @human = Factory.build(:human_player, :name => "Bob")
-      flexmock(HumanPlayer).should_receive(:new).
-        with("name" => @human.name).
-        and_return(@human).once
-      flexmock(Game, :new => @game)
+    let!(:game) { Factory.build(:game) }
+    let!(:human) { Factory.build(:human_player, :name => "Bob") }
+    let(:params) { { :game => { :name => human.name } } }
 
+    before do
+      flexmock(HumanPlayer).should_receive(:new).
+        with("name" => human.name).
+        and_return(human).once
+      flexmock(Game, :new => game)
     end
-    subject { @game }
+
+    def post_to_create
+      post :create, params
+    end
+
+    subject { game }
 
     context "with good save" do
-      before do
-        post :create, :game => { :name => @human.name }
-      end
+      before { post_to_create }
 
-      its(:players) { should include(@human) }
-      it { response.should redirect_to(game_players_path(@game)) }
+      its(:players) { should include(human) }
+      it { response.should redirect_to(game_players_path(game)) }
     end
 
     context "with bad save on game" do
       before do
-        make_invalid(@game)
-        post :create, :game => { :name => @human.name }
+        make_invalid(game)
+        post_to_create
       end
       it { response.should redirect_to(new_game_path) }
       it "sets the flash error message" do
@@ -42,8 +47,8 @@ describe GamesController do
 
     context "with bad save on human" do
       before do
-        make_invalid(@human, "boogey man")
-        post :create, :game => { :name => @human.name }
+        make_invalid(human, "boogey man")
+        post_to_create
       end
       it { response.should redirect_to(new_game_path) }
       it "flash message should include human error messages" do
--- labs/greed/spec/controllers/players_controller_spec.rb
+++ labs/greed/spec/controllers/players_controller_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe PlayersController do
 
+  describe "GET index" do
+    let!(:game) { :game_set }
+    it "" do
+#      get :index, :game_id => @game.id
+    end
+  end
+
 end

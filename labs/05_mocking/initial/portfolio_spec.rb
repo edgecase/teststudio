8b27cb8f669fac2c25331bd245e292585c3c4f1f
Jim Weirich <jim.weirich@gmail.com>
2010-10-20 15:54:25 -0400

    updated lab 05 initial directory.

---
+++ labs/05_mocking/initial/portfolio_spec.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby -wKU
+
+require 'flexmock/rspec'
+
+require 'quote_service'
+require 'portfolio'
+
+describe Portfolio do
+  context "with a single stock" do
+    it "returns just that stack value"
+  end
+
+  context "with serveral stocks" do
+    it "returns the sum of the stock values"
+  end
+
+  context "with a bad login" do
+    it "returns nil"
+  end
+
+  context "with a logout failure" do
+    it "still returns a good value"
+  end
+
+  context "with a successful login" do
+    it "must call logout"
+  end
+
+  # NOTE: We don't have an explicit specifications for
+  # login/quote/logout ordering.  Generally that's covered as part of
+  # the good path testing. If you feel you need an explicit
+  # specification for that, feel free to add that spec.
+end

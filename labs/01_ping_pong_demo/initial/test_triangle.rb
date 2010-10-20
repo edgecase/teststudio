beded5cba1cd0ccc1aecc4bf1cce2d5558c4be08
Jim Weirich <jim.weirich@gmail.com>
2010-10-20 11:41:08 -0400

    moved keynote files and pdfs

--- labs/01_ping_pong_demo/initial/test_triangle.rb
+++ labs/01_ping_pong_demo/initial/test_triangle.rb
@@ -1,17 +1,12 @@
 #!/usr/bin/env ruby
 
-require 'test/unit'
-
 def classify_triangle(one, two, three)
 
 end
 
-class TriangleTest < Test::Unit::TestCase
-  def test_something
-    # fill in this test
+describe "#classify_triangle" do
+  it "does something" do
+    # Star
   end
-
-  # create more tests.  
-
 end
 

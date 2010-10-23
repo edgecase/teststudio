fed5cb3fd076dbcd04d5743f560ad112affadf51
Jim Weirich <jim.weirich@gmail.com>
2010-10-22 22:37:48 -0400

    Removed COVER_ME env guard

--- labs/greed/spec/spec_helper.rb
+++ labs/greed/spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require 'cover_me' if ENV['COVER_ME']
+require 'cover_me'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)

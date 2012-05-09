94bc3247469e636b331289427c74e13c5dc0bee3
Jim Weirich <jim.weirich@gmail.com>
2012-05-09 13:40:49 -0700

    Removed test and code for adding games

--- labs/08_integration_testing/initial/greed/app/controllers/games_controller.rb
+++ labs/08_integration_testing/initial/greed/app/controllers/games_controller.rb
@@ -1,32 +1,11 @@
 class GamesController < ApplicationController
-  def new
-    setup_for_new
-  end
 
-  def create
-    if params[:winner_id].blank? || params[:loser_id].blank?
-      flash.now[:error] = "Both winner and loser must be specified"
-      setup_for_new
-      render :new
-    elsif params[:winner_id] == params[:loser_id]
-      flash.now[:error] = "The winner and loser must be different"
-      setup_for_new
-      render :new
-    else
-      winner = Member.find(params[:winner_id])
-      loser = Member.find(params[:loser_id])
-      game = Game.new(winner: winner, loser: loser)
-      game.update_ranks
-      game.save
-      redirect_to members_path, alert: "Game recorded: Winner is #{winner.name}, Loser is #{loser.name}"
-    end
-  end
+  # def new
+  #   IMPLEMENT THIS
+  # end
 
-  private
-
-  def setup_for_new
-    @game = Game.new
-    @members = Member.by_name
-  end
+  # def create
+  #   IMPLEMENT THIS
+  # end
 
 end
